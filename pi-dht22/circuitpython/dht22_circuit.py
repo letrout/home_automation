879138b661d098b071ed5f840bd5ce8c97566634
Joel Luth <joelluth@gmail.com>
2021-04-27 13:50:18 -0500

    first attempt

---
+++ pi-dht22/circuitpython/dht22_circuit.py
@@ -0,0 +1,64 @@
+# A script to poll DHT22 for temp/humidity
+# TODO: publish to MQTT
+
+import adafruit_dht
+import board
+import RPi.GPIO as GPIO
+from datetime import datetime
+import getopt
+import sys
+import time
+
+#DHT_SENSOR = Adafruit_DHT.DHT22
+PROBE_NAME = "PI4"
+DEFAULT_INT = 2
+DEFAULT_PIN = 4
+
+def poll_dht22(dht_device):
+    hum = None
+    temp = None
+    try:
+        #hum, temp = adafruit_dht.read_retry(DHT_SENSOR, pin)
+        temp = dht_device.temperature
+        hum = dht_device.humidity
+    except RuntimeError as error:
+        print(error.args[0])
+    return (hum, temp)
+
+def print_loop(interval, dht_device):
+    while True:
+        #print(f'getting data pin {pin}')
+        hum, temp = poll_dht22(dht_device)
+        print('got data')
+        if hum is not None and temp is not None:
+            print(f'{datetime.now()} - T={c_to_f(temp):0.1f}F H={hum:0.1f}%')
+        else:
+            print("Failed to retrieve data from humidity sensor")
+        #break
+        time.sleep(interval)
+
+def c_to_f(temp_c):
+    return temp_c * (9 / 5) + 32
+
+def main(argv):
+    #GPIO.setmode(GPIO.BCM)
+    interval = DEFAULT_INT
+    pin = DEFAULT_PIN
+    try:
+        opts, args = getopt.getopt(argv,"hi:p:",["interval=","pin="])
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('dht22.py -s <seconds between readings> -p <GPIO pin>')
+            sys.exit()
+        elif opt in ("-i", "--interval"):
+            interval = int(arg)
+        elif opt in ("-p", "--pin"):
+            pin = int(arg)
+
+    dht_device = adafruit_dht.DHT22(board.D4)
+    print_loop(interval, dht_device)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
