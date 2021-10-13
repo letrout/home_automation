925ee21f3f074c447e2056a79e689e2339faacd8
Joel Luth <33353303+letrout@users.noreply.github.com>
2021-10-13 15:40:47 -0500

    InfluxDB (#7)
    * script to run influxdb2 docker container
    
    * add --name and --net to simplify connection to telegraf
    
    * telegraf stuff
    
    * generated telegraf.conf
    
    * add info about API tokens
    
    * update telegraf README
    
    * add mounts to allow host monitoring
    
    * add influxdb_v2 output to telelgraf conf
    
    * add command for testing the telegraf container
    
    * add '--restart unless-stopped' to docker run
    
    * create two new buckets, "infra" and "weather"
    
    These will hold infrastructure system data, and weather data, respectively.
    Add filtering for expected inputs.
    
    * pass influxdb tokens as env vars wasn't working (too many weird chars)
    
    * add openweathermap input
    
    * remove some undesired inputs
    
    * remove unused influxdb v1 output
    
    * fix influxdb2 output host name
    
    * change "openweather" name pass to "weather"
    
    * add logging
    
    * add docker tracking
    
    * iadd mqtt_consumer input

---
+++ docker/influxdb2/influx_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+docker network create influxdb2
+docker run -d -p 8086:8086 \
+      --restart unless-stopped \
+      --name=influxdb2 \
+      --net=influxdb2 \
+      -v /docker/influxdb2/data:/var/lib/influxdb2 \
+      -v /docker/influxdb2/config:/etc/influxdb2 \
+      -e DOCKER_INFLUXDB_INIT_MODE=setup \
+      -e DOCKER_INFLUXDB_INIT_USERNAME=$INFLUX_USER \
+      -e DOCKER_INFLUXDB_INIT_PASSWORD=$INFLUX_PW \
+      -e DOCKER_INFLUXDB_INIT_ORG=$INFLUX_ORG \
+      -e DOCKER_INFLUXDB_INIT_BUCKET=$INFLUX_BUCKET \
+      influxdb:2.0-alpine
