86ae665b956b93d4c015d91bcc2f8f0eaed4bf2c
Joel Luth <joelluth@gmail.com>
2021-10-13 22:43:34 -0500

    add some tests

--- tests/test_influx.py
+++ tests/test_influx.py
@@ -8,5 +8,46 @@ def test_lp_1f1v():
         "temp",
         {"field1": 1},
         {"tag1": 2},
-        1634158455045502066)
+        1634158455045502066
+        )
     assert test == "temp tag1=2 field1=1 1634158455045502066"
+
+
+def test_lp_2f2v():
+    test = influx.influx_lp(
+        "temp",
+        {"field1": 1, "field2": 5.5},
+        {"tag1": 2, "tag2": 6},
+        1634158455045502066
+        )
+    assert test == "temp tag1=2,tag2=6 field1=1,field2=5.5 1634158455045502066"
+
+
+def test_lp_bad_ts():
+    test = influx.influx_lp(
+        "temp",
+        {"field1": 1},
+        {"tag1": 2},
+        "manynanoseconds"
+        )
+    assert test is None
+
+
+def test_lp_bad_field():
+    test = influx.influx_lp(
+        "temp",
+        ["field1", 1],
+        {"tag1": 2},
+        1634158455045502066
+        )
+    assert test is None
+
+
+def test_lp_bad_tag():
+    test = influx.influx_lp(
+        "temp",
+        {"field1": 1},
+        "tag1",
+        1634158455045502066
+        )
+    assert test is None
