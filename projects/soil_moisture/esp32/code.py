719b7ed3604ddc6bb967102f6927210f6815ba05
Joel Luth <joelluth@gmail.com>
2021-12-27 22:15:03 -0600

    read a capacitive soil moisture probe from ADC pin

---
+++ projects/soil_moisture/esp32/code.py
@@ -0,0 +1,33 @@
+import time
+import board
+from analogio import AnalogIn
+from ulab.numpy import interp
+
+ADC_BITS = 16
+V_REF = 3.3
+ADC_PIN = "A1"
+# DRY and WET values are empirically determined for
+# each individual soil probe
+BITS_DRY = 52586 # ADC value when probe in air
+BITS_WET = 29113 # ADC value when probe submersed in water
+
+analog_in = AnalogIn(eval(f'board.{ADC_PIN}'))
+#analog_in = AnalogIn(board.A1)
+
+
+def get_voltage(pin):
+    return (pin.value * V_REF) / ((1 << ADC_BITS) -1)
+
+
+def dry_pct(bits):
+    return interp(bits, [BITS_WET, BITS_DRY], [0,100])[0]
+
+
+while True:
+    #print((get_voltage(analog_in),))
+    #analog_in = AnalogIn(eval(f'board.{ADC_PIN}'))
+    bits = analog_in.value
+    dry = dry_pct(bits)
+    print(f"{ADC_PIN}: {bits} bits, dry: {dry:.1f}%")
+    time.sleep(5)
+
