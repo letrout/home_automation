168a5668b58fe993669ce068ca1a9d74d4351843
Joel Luth <joelluth@gmail.com>
2021-12-29 10:08:59 -0600

    pylint fixes

--- projects/soil_moisture/esp32/my_ntp.py
+++ projects/soil_moisture/esp32/my_ntp.py
@@ -5,12 +5,13 @@ from https://www.mattcrampton.com/blog/query_an_ntp_server_from_python/
 """
 
 import rtc
-import socketpool
-import struct, time
+import struct
+import time
 
 import my_wifi
- 
-def getNTPTime(pool, host="pool.ntp.org"):
+
+
+def get_ntp_sec(pool, host="pool.ntp.org"):
     """
     Get the current time from NTP server
     params:
@@ -21,23 +22,24 @@ def getNTPTime(pool, host="pool.ntp.org"):
     """
     port = 123
     buf = 48
-    address = (host,port)
+    address = (host, port)
     msg = '\x1b' + 47 * '\0'
     recv_msg = bytearray(buf)
- 
+
     # reference time (in seconds since 1900-01-01 00:00:00)
-    TIME1970 = 2208988800 # 1970-01-01 00:00:00
- 
+    time_1970 = 2208988800 # 1970-01-01 00:00:00
+
     # connect to server
-    client = pool.socket( pool.AF_INET, pool.SOCK_DGRAM)
+    client = pool.socket(pool.AF_INET, pool.SOCK_DGRAM)
     client.sendto(msg.encode('utf-8'), address)
     #msg, address = client.recv_into( recv_msg, buf )
-    client.recv_into( recv_msg, buf )
- 
-    t = struct.unpack( "!12I", recv_msg )[10]
-    t -= TIME1970
+    client.recv_into(recv_msg, buf)
+
+    ntp_sec = struct.unpack("!12I", recv_msg)[10]
+    ntp_sec -= time_1970
     #return time.ctime(t).replace("  "," ")
-    return t
+    return ntp_sec
+
 
 def set_rtc_to_ntp(pool, host="pool.ntp.org"):
     """
@@ -48,13 +50,5 @@ def set_rtc_to_ntp(pool, host="pool.ntp.org"):
     return:
         nothing
     """
-    t = getNTPTime(pool, host)
-    rtc.RTC().datetime = time.localtime(t)
- 
-if __name__ == "__main__":
-    my_wifi.connect()
-    pool = socketpool.SocketPool(my_wifi.wifi.radio)
-    t = getNTPTime(pool)
-    print(time.localtime())
-    rtc.RTC().datetime = time.localtime(t)
-    print(time.localtime())
+    ntp_sec = get_ntp_sec(pool, host)
+    rtc.RTC().datetime = time.localtime(ntp_sec)
