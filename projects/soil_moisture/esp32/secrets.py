60a7c9c5760a820ded495a8b58ebe2265673bf6e
Joel Luth <joelluth@gmail.com>
2021-12-28 20:01:34 -0600

    add MQTT params to secrets

--- projects/soil_moisture/esp32/secrets.py
+++ projects/soil_moisture/esp32/secrets.py
@@ -1,6 +1,10 @@
 secrets = {
     'ssid' : 'SSID',
     'password' : 'WIFI_PW',
+    'mqtt_broker' : 'MQTT_BROKER',
+    'mqtt_user' : 'MQTT_USER',
+    'mqtt_password' : 'MQTT_PASSORD',
+    'mqtt_port' : 'MQTT_PORT',
     'aio_username' : 'my_adafruit_io_username',
     'aio_key' : 'my_adafruit_io_key',
     'timezone' : "America/Chicago", # http://worldtimeapi.org/timezones
