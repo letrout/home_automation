7745d2957de5b07e812db2e9a63d060dd7a26ae7
Joel Luth <joelluth@gmail.com>
2021-12-30 12:56:29 -0600

    flake8 fixes

--- projects/soil_moisture/esp32/test_ntp.py
+++ projects/soil_moisture/esp32/test_ntp.py
@@ -4,7 +4,6 @@ rename to code.py for running on uC
 """
 
 import socketpool
-import rtc
 import time
 
 import my_ntp
@@ -16,12 +15,12 @@ def main():
     pool = socketpool.SocketPool(my_wifi.wifi.radio)
     ntp_sec = my_ntp.get_ntp_sec(pool)
     print("before ntp:\t%s " % time.localtime())
-    #rtc.RTC().datetime = time.localtime(ntp_sec)
     if my_ntp.set_rtc_to_ntp(pool) == 0:
         print("ntp seconds %d   " % ntp_sec)
         print("after ntp:\t%s" % time.localtime())
     else:
         print("ERROR setting RTC to NTP")
+
 
 if __name__ == "__main__":
     main()
