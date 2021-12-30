652fa41c6ac5c45c9f0ffcf43577b78d010c43d8
Joel Luth <joelluth@gmail.com>
2021-12-30 13:03:24 -0600

    flake8 fixes

--- projects/soil_moisture/esp32/code.py
+++ projects/soil_moisture/esp32/code.py
@@ -8,30 +8,37 @@ V_REF = 3.3
 ADC_PIN = "A1"
 # DRY and WET values are empirically determined for
 # each individual soil probe
-BITS_DRY = 52586 # ADC value when probe in air
-BITS_WET = 29113 # ADC value when probe submersed in water
+BITS_DRY = 52586    # ADC value when probe in air
+BITS_WET = 29113    # ADC value when probe submersed in water
 
 analog_in = AnalogIn(eval(f'board.{ADC_PIN}'))
-#analog_in = AnalogIn(board.A1)
 
 
 def get_voltage(pin):
-    return (pin.value * V_REF) / ((1 << ADC_BITS) -1)
+    """
+    Get voltage value from an ADV pi n
+    """
+    return (pin.value * V_REF) / ((1 << ADC_BITS) - 1)
 
 
 def dry_pct(bits):
-    return interp(bits, [BITS_WET, BITS_DRY], [0,100])[0]
+    """
+    The percentage of "wet" into the probe's range
+    """
+    return interp(bits, [BITS_WET, BITS_DRY], [0, 100])[0]
 
 
 def wet_pct(bits):
+    """
+    The percentage of "wet" into the probe's range
+    """
     return 100.0 - dry_pct(bits)
 
 
 while True:
-    #print((get_voltage(analog_in),))
-    #analog_in = AnalogIn(eval(f'board.{ADC_PIN}'))
+    # print((get_voltage(analog_in),))
+    # analog_in = AnalogIn(eval(f'board.{ADC_PIN}'))
     bits = analog_in.value
     dry = dry_pct(bits)
     print(f"{ADC_PIN}: {bits} bits, dry: {dry:.1f}%")
     time.sleep(5)
-
