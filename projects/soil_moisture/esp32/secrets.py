b5b7471e40ec4a7548b12179934ba307cea47434
Joel Luth <joelluth@gmail.com>
2022-01-02 11:36:27 -0600

    add plants topic to telegraf

--- projects/soil_moisture/esp32/secrets.py
+++ projects/soil_moisture/esp32/secrets.py
@@ -5,7 +5,7 @@ secrets = {
     'mqtt_user' : 'MQTT_USER',
     'mqtt_password' : 'MQTT_PASSORD',
     'mqtt_port' : 'MQTT_PORT',
-    'mqtt_topic' : 'MQTT_BASE_TOPIC',
+    'mqtt_topic' : 'influx/Owens/plants/',
     'aio_username' : 'my_adafruit_io_username',
     'aio_key' : 'my_adafruit_io_key',
     'timezone' : "America/Chicago", # http://worldtimeapi.org/timezones
