53848121376371547f7385d5ebee7ce0b793609b
Joel Luth <joelluth@gmail.com>
2022-03-28 11:13:33 -0500

    update time with NTP
    might be wise to timestamp our door events at the source

--- projects/door_monitor/door_monitor.ino
+++ projects/door_monitor/door_monitor.ino
@@ -1,12 +1,23 @@
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 
 #include "secrets.h"
 
-const int door_pin = D1;
+// From secrets.h
 const char* ssid     = STASSID;
 const char* password = STAPSK;
+const char* ntp_server = NTP_SERVER;
 
+const int door_pin = D1;
 int door_state; // 0 - closed, 1 - open
+const long utcOffsetInSeconds = 0;
+const unsigned long ntp_update_ms = 30 * 60 * 1000L; // NTP update interval ms
+unsigned long ntp_last_ms = 0L;
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, ntp_server, utcOffsetInSeconds);
 
 void setup()
 {
@@ -15,8 +26,8 @@ void setup()
 
   pinMode(door_pin, INPUT_PULLUP);
 
+  // WiFi
   WiFi.begin(ssid, password);
-
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -24,14 +35,26 @@ void setup()
     Serial.print(".");
   }
   Serial.println();
-
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+  // NTP
+  timeClient.begin();
+  // Not sure why, but seem to need a little delay before updating time
+  delay(1000);
+  timeClient.update();
+  ntp_last_ms = millis();
 }
 
 void loop() {
+  unsigned long now = millis();
   // Serial.print("Connected, IP address: ");
   // Serial.println(WiFi.localIP());
+  if ((now - ntp_last_ms) > ntp_update_ms) {
+    ntp_last_ms = now;
+    timeClient.update();
+  }
+  //print_time();
 
   door_state = digitalRead(door_pin);
   if (door_state == HIGH) {
@@ -40,6 +63,13 @@ void loop() {
     Serial.println("Door closed");
   }
 
-  delay(5000);
-  // new line
+  delay(1000);
+}
+
+void print_time() {
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
 }
