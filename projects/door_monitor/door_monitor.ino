abdb1fb8b711ce63ed7b8333d4579eff373725d1
Joel Luth <joelluth@gmail.com>
2022-03-27 22:43:12 -0500

    add door switch sensor

--- projects/door_monitor/door_monitor.ino
+++ projects/door_monitor/door_monitor.ino
@@ -2,13 +2,18 @@
 
 #include "secrets.h"
 
+const int door_pin = D1;
 const char* ssid     = STASSID;
 const char* password = STAPSK;
+
+int door_state; // 0 - closed, 1 - open
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
+
+  pinMode(door_pin, INPUT_PULLUP);
 
   WiFi.begin(ssid, password);
 
@@ -25,8 +30,15 @@ void setup()
 }
 
 void loop() {
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
+  // Serial.print("Connected, IP address: ");
+  // Serial.println(WiFi.localIP());
+
+  door_state = digitalRead(door_pin);
+  if (door_state == HIGH) {
+    Serial.println("Door open");
+  } else {
+    Serial.println("Door closed");
+  }
 
   delay(5000);
   // new line
