e1f51becef09d1024b2f7b36935d845b51578507
Joel Luth <33353303+letrout@users.noreply.github.com>
2022-04-02 11:14:24 -0500

    door monitor (#28)
    * initial ESP8266 with WiFi
    
    * add door switch sensor
    
    * update time with NTP
    
    might be wise to timestamp our door events at the source
    
    * add MQTT publish
    
    * only publish state change to mqtt
    
    * change door states to booleans
    
    * add timestamp to mqtt msg
    
    should be more accurate than relying on subscribers to set the time (especially if we make these sticky)
    
    * retain last mqtt message
    
    * initialize door state to invalid value
    
    if we publish only changed values, there is a chance we won't see the initial bootup value if it is the same as the initialized value. So initialize to something that is sure to change.

---
+++ projects/door_monitor/door_monitor.ino
@@ -0,0 +1,147 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <string.h>
+
+#include "secrets.h"
+
+#define ARRAY_LENGTH(array) (sizeof(array)/sizeof((array)[0]))
+
+// From secrets.h
+const char* ssid     = STASSID;
+const char* password = STAPSK;
+const char* ntp_server = NTP_SERVER;
+const char* mqtt_broker = MQTT_BROKER;
+const int mqtt_port = MQTT_PORT;
+const char* mqtt_username = MQTT_USER;
+const char* mqtt_password = MQTT_PASSWORD;
+
+const int door_pin = D1;
+const char* door_name = "front";
+int door_state = -1; // 0 - closed, 1 - open
+int door_last_state = -1; // initialize to invalid state
+const long utcOffsetInSeconds = 0;
+const unsigned long ntp_update_ms = 30 * 60 * 1000L; // NTP update interval ms
+unsigned long ntp_last_ms = 0L;
+char client_id[16] = "d1-"; // will be the MQTT client ID, after MAC appended
+const char* measurement = "doors";
+const char* topic = "events";
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, ntp_server, utcOffsetInSeconds);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  pinMode(door_pin, INPUT_PULLUP);
+
+  // WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // NTP
+  timeClient.begin();
+  // Not sure why, but seem to need a little delay before updating time
+  delay(1000);
+  timeClient.update();
+  ntp_last_ms = millis();
+
+  //MQTT
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  // append our MAC to client_id so it's unique
+  byte bmac[6];
+  WiFi.macAddress(bmac);
+  for (byte i = 0; i < ARRAY_LENGTH(bmac); ++i) {
+    char buf[3];
+    sprintf(buf, "%02x", bmac[i]);
+    strncat(client_id, buf, 3);
+  }
+  while (!client.connected()) {
+    Serial.printf("client %s connecting to mqtt broker...\n", client_id);
+    if (client.connect(client_id, mqtt_username, mqtt_password)) {
+      Serial.println("mqtt broker connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+} // setup()
+
+void loop() {
+  unsigned long now = millis();
+  char mqtt_msg [128];
+  // Serial.print("Connected, IP address: ");
+  // Serial.println(WiFi.localIP());
+  if ((now - ntp_last_ms) > ntp_update_ms) {
+    ntp_last_ms = now;
+    timeClient.update();
+  }
+  //print_time();
+
+  door_state = digitalRead(door_pin);
+  if (door_state == HIGH) {
+    Serial.println("Door open");
+  } else {
+    Serial.println("Door closed");
+  }
+  // Publish changes to MQTT
+  // publish all open events? or just changes?
+  if (door_state != door_last_state) {
+    sprintf(mqtt_msg, "%s,door=%s state=%d %lu%s", measurement, door_name, door_state, timeClient.getEpochTime(), "000000000");
+    int len = strlen(mqtt_msg) + 1;
+    client.publish(topic, (uint8_t*)mqtt_msg, len, true);
+    // client.publish(topic, mqtt_msg);
+    memset(mqtt_msg, 0, sizeof mqtt_msg);
+  }
+  door_last_state = door_state;
+
+  client.loop();
+  delay(1000);
+} // loop()
+
+void print_time() {
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char) payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+  
+  /* example: extract the "temp_f" field from mqtt msg */
+  /*
+  float value;
+  int ret;
+  ret = get_mqtt_val("temp_f", payload, length, &value);
+  if (ret == 0) {
+    Serial.print("value: ");
+    Serial.println(value);
+  }
+  */
+}
