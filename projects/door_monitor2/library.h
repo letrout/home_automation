4daf1a0fa9f1c705f5961d7d0ec754918cabca22
Joel Luth <joelluth@gmail.com>
2022-04-11 22:05:28 -0500

    add header guard

--- projects/door_monitor2/library.h
+++ projects/door_monitor2/library.h
@@ -1,5 +1,11 @@
+#ifndef SENSOR_DEVICE_H
+#define SENSOR_DEVICE_H 1
+
+
 const int door_pin = D1;
 const char* location = "Owens";
 const char* room = "library";
 const char* room_loc = "front";
 const char* type = "door";
+
+#endif
