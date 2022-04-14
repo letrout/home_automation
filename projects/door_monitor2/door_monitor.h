c56efa276c2d23c1342d8d64c1bd08e99d7aa5f4
Joel Luth <33353303+letrout@users.noreply.github.com>
2022-04-14 15:39:12 -0500

    New door monitor sketch (#29)
    * update for garage
    
    * new door monitor
    
    copy of door_monitor
    
    * move device-specific params to header file
    
    plan is to store config data for each specific device into a header file, then create a header for each device with common code in .ino.
    
    * mqtt msg was including null terminator
    
    Reduce the length of the mqtt msg by 1, so it drops the string null terminator. Having the null terminator in the mqtt msg was causing a parse error in telegraf.
    
    * add header files for more devices
    
    * add header guard
    
    * add header guard
    
    * remove comments
    
    * add a door_monitor header file
    
    * define the room in header file
    
    might be useful?
    
    * reconnect mqtt client
    
    Before publish, check mqtt broker connection and reconnect if necessary
    Tested by restarting mqtt broker, that used to kill publishing but now it continues
    
    * WiFi autoreconnect
    
    Not tested
    from https://randomnerdtutorials.com/solved-reconnect-esp8266-nodemcu-to-wifi/

---
+++ projects/door_monitor2/door_monitor.h
@@ -0,0 +1,30 @@
+#ifndef DOOR_MONITOR_H
+#define DOOR_MONITOR_H 1
+
+
+/**
+ * @brief print the current time to serial
+ * 
+ */
+void print_time(void);
+
+
+/**
+ * @brief MQTT subscribe callback function
+ * 
+ * @param topic the topic of the message
+ * @param payload the message payload
+ * @param length length of the message payload
+ */
+void callback(char *topic, byte *payload, unsigned int length);
+
+
+/**
+ * @brief check if connected to mqtt broker, reconnect loop if necessary
+ * This is blocking. Right now this app only exists to publish to MQTT,
+ * but if that changes this will need to change to something non-blocking
+ * 
+ */
+void mqtt_reconnect(void);
+
+#endif
---
+++ projects/door_monitor2/garage.h
@@ -0,0 +1,12 @@
+#ifndef SENSOR_DEVICE_H
+#define SENSOR_DEVICE_H 1
+#define GARAGE 1
+
+
+const int door_pin = D1;
+const char* location = "Owens";
+const char* room = "garage";
+const char* room_loc = "side";
+const char* type = "door";
+
+#endif
