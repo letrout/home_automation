228ddb406c74c824da524b7a0740979f63fc1d16
Joel Luth <33353303+letrout@users.noreply.github.com>
2022-07-11 20:57:33 -0500

    Funhouse (#32)
    * copy sensortest to mbr
    
    * add timers
    
    * move sensor variables to global
    
    move the sensor variables out of loop() scope to global, as we may (depending on timers) need to use their values from previous iteration
    
    * shift come more variables around/scope
    
    * read sensors in a function
    
    collect the sensor reads (except SCD40 which uses a different timer) into a function. Makes it easier to trigerr all the reads from one timer.
    
    * move SGP30 measurements to read_sensors()
    
    * add function docstrings
    
    * move sensor display generation to a function
    
    Collecting the display in one place should make it easier to control (ie sleep the display off, trigger on from button push)
    
    * pass and return current cursor value
    
    allows us to start the display from a certain point, and tell the caller where the display now ends
    
    * add function declarations to header
    
    Not needed in Arduino, but makes for more correct cpp and cleans up the main file a bit (moves out the fucntion docstrings)
    
    * move sensor serial output to read_sensors()
    
    so it happens on the timer to read the sensors
    
    * move mqtt publish to function
    
    * remove the scd4x-based timer
    
    Now that read_scd4x() fires off it's own timer, don't need to delay the entire loop on that timer
    
    * move SCD40 read up
    
    display update and MQTT publish should happen after scd40 read
    
    * turn on display with button push
    
    Display normally off, turn on with button push
    
    * set dotstar brightness based on ambient light level
    
    * set middle dotstar hue to co2 level
    
    * mqtt sub to pepper plant moisture
    
    * set dotstar hue based on pepper plant moisture levels
    
    * useless changes
    
    * remove pixels.setBrightness() from loop()
    
    setBrightness() is only supposed to be used once, in setup()
    see: https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
    
    * reverse the dotstar hue mapping
    
    This feels wrong, but seems to produce the correct results.
    
    * reconnect wifi and mqtt
    
    Make WiFi persistent/autoreconnect, and check/reconnect mqtt client before publish
    
    * home_assistant configuration.yaml
    
    store home assistant config

---
+++ projects/funhouse/funhouse_mbr/funhouse_mbr.h
@@ -0,0 +1,110 @@
+#ifndef __SparkFun_SCD4x_ARDUINO_LIBARARY_H__
+#include <SensirionI2CScd4x.h>
+#endif
+
+/**
+ * @brief Read the sensors
+ * store results in global variables
+ */
+void read_sensors(void);
+
+
+/**
+ * @brief Read the SCD40 sensor
+ * store the results in global variables
+ * 
+ * @return uint16_t error code returned from sensor readMeasurement()
+ */
+uint16_t read_scd4x(void);
+
+
+/**
+ * @brief output a tone to the speaker
+ * 
+ * @param pin 
+ * @param frequency 
+ * @param duration 
+ */
+void tone(uint8_t pin, float frequency, float duration);
+
+
+/**
+ * @brief Set the up scd4x object
+ * 
+ * @param scd4x SCD40 object
+ * @return uint16_t error value
+ */
+uint16_t setup_scd4x(SensirionI2CScd4x& scd4x);
+
+
+/**
+ * @brief serial print an int in hexadecimal
+ * 
+ * @param value the integer value to be printed
+ */
+void printUint16Hex(uint16_t value);
+
+
+/**
+ * @brief serial print a serial number
+ * 
+ * @param serial0 
+ * @param serial1 
+ * @param serial2 
+ */
+void printSerialNumber(uint16_t serial0, uint16_t serial1, uint16_t serial2);
+
+
+/**
+ * @brief return absolute humidity [mg/m^3] with approximation formula
+ * 
+ * @param temperature temperature [°C]
+ * @param humidity humidity [%RH]
+ * @return uint32_t humidity in mg/m^3
+ */
+uint32_t getAbsoluteHumidity(float temperature, float humidity);
+
+
+/**
+ * @brief MQTT subscribe callback function
+ * 
+ * @param topic the topic of the message
+ * @param payload the message payload
+ * @param length length of the message payload
+ */
+void callback(char *topic, byte *payload, unsigned int length);
+
+
+/**
+ * @brief display sensor values on the TFT
+ * 
+ * @param uint8_t starting cursor position
+ * @return uint8_t the final cursor value
+ */
+uint8_t display_sensors(const uint8_t cursor_y_start = 0);
+
+
+/**
+ * @brief publish sensor readings to MQTT broker
+ * 
+ */
+void mqtt_pub_sensors(void);
+
+
+/**
+ * @brief Get the pepper plant moisture level from MQTT
+ * 
+ * @param payload MQTT payload
+ * @param length length of payload
+ * @return int8_t error code (0 on success)
+ */
+int8_t get_pepper_mqtt(const byte* payload, const int length);
+
+
+/**
+ * @brief check if connected to mqtt broker, reconnect loop if necessary
+ * This is blocking. Right now this app only exists to publish to MQTT,
+ * but if that changes this will need to change to something non-blocking
+ * 
+ */
+void mqtt_reconnect(void);
