e86a45acf089c9fa9de018d1b7e42c1a25373299
Joel Luth <joelluth@gmail.com>
2022-07-16 00:01:01 -0500

    add check for new dps310 temp/pressure data before read

--- projects/funhouse/funhouse_mbr/fh_sensors.ino
+++ projects/funhouse/funhouse_mbr/fh_sensors.ino
@@ -1,8 +1,10 @@
 #include "fh_sensors.h"
 
+Adafruit_DPS310 dps;
+
 uint8_t setup_dps310() {
+  Serial.println("INSIDE setup_dps310()");
   uint8_t retval = 1;
-  delay(10000);
   for (uint8_t i = 0; i++; i < 5 ) {
     if (dps.begin_I2C()) {
       Serial.println("connected to DPS310...");
@@ -24,13 +26,20 @@ uint8_t setup_dps310() {
 
 uint8_t read_dps310() {
   sensors_event_t t, p;
-  if (dps.getEvents(&t, &p)) {
-    Serial.printf("DPS310 temp %f C, %f kpa\n", t.temperature, p.pressure);
-    dps_temp = t;
-    dps_pressure = p;
-    return 0;
+  setup_dps310();
+  if (dps.pressureAvailable() && dps.temperatureAvailable()) {
+    Serial.println("DPS310 new temp and pressure available...");
+    if (dps.getEvents(&t, &p)) {
+      Serial.printf("DPS310 temp %f C, %f kpa\n", t.temperature, p.pressure);
+      dps_temp = t;
+      dps_pressure = p;
+      return 0;
+    } else {
+      Serial.println("DPS310 read failed!");
+      return 1;
+    }
   } else {
-    Serial.println("DPS310 read failed!");
+    Serial.println("DPS310 temp or pressure not available");
     return 1;
   }
 }
