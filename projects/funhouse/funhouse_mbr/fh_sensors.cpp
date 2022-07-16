66b93a3d5a0af44ad257193086b806de0827d7b9
Joel Luth <joelluth@gmail.com>
2022-07-16 17:24:00 -0500

    derivative class of Adafruit_DPS310

--- projects/funhouse/funhouse_mbr/fh_sensors.cpp
+++ projects/funhouse/funhouse_mbr/fh_sensors.cpp
@@ -7,6 +7,45 @@ Adafruit_DPS310 dps;
 // DPS310
 sensors_event_t dps_temp, dps_pressure;
 
+// DPS310
+uint8_t FhDps310::setup_dps310(void) {
+  uint8_t i;
+  uint8_t retval = 1;
+  for (i = 1; i++; i <= 5 ) {
+    if (begin_I2C()) {
+      retval = 0;
+      break;
+    } else {
+      Serial.println("Connect to DPS310 FAILED!");
+      delay(100);
+    }
+  }
+  if (retval == 0) {
+    configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
+    configureTemperature(DPS310_64HZ, DPS310_64SAMPLES);
+  }
+  return retval;
+}
+
+uint8_t FhDps310::read_dps310(void) {
+  sensors_event_t t, p;
+  if (pressureAvailable() && temperatureAvailable()) {
+    if (getEvents(&t, &p)) {
+      _last_temp_c = t.temperature;
+      _last_press_hpa = p.pressure;
+      _last_read_ms = millis();
+      return 0;
+    } else {
+      Serial.println("DPS310 read failed!");
+      return 1;
+    }
+  } else {
+    Serial.println("DPS310 temp or pressure not available");
+    return 2;
+  }
+}
+// End DPS310
+
 uint8_t setup_dps310() {
   uint8_t i;
   uint8_t retval = 1;
--- projects/funhouse/funhouse_mbr/fh_sensors.h
+++ projects/funhouse/funhouse_mbr/fh_sensors.h
@@ -11,6 +11,37 @@ extern Adafruit_DPS310 dps;
 // DPS310
 extern sensors_event_t dps_temp, dps_pressure;
 
+/**
+ * @brief Class to extend Adafruit_DPS310
+ * 
+ */
+class FhDps310 : public Adafruit_DPS310 {
+  private:
+    float _last_temp_c;
+    float _last_press_hpa;
+    unsigned long _last_read_ms;
+
+  public:
+    FhDps310();
+    const float & last_temp_c()     { return _last_temp_c; }
+    const float & last_press_hpa()  { return _last_press_hpa; }
+    const unsigned long & last_read_ms()  { return _last_read_ms; }
+
+    /**
+     * @brief Initialize the dps310 object
+     * 
+     * @return uint8_t 0 on success
+     */
+    uint8_t setup_dps310();
+
+    /**
+     * @brief Read values from the DPS310
+     * 
+     * @return uint8_t 0 on success, 1 on failure, 2 if new values not available
+     */
+    uint8_t read_dps310();
+};
+
 
 /**
  * @brief Initialize the dps310 object
