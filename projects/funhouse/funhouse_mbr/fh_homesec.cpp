309157b5ea481204fadef4ebf4ec2d8044d0966e
Joel Luth <joelluth@gmail.com>
2022-08-27 20:16:05 -0500

    initial file creation

---
+++ projects/funhouse/funhouse_mbr/fh_homesec.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file fh_homesec.cpp
+ * @author Joel Luth (joel.luth@gmail.com)
+ * @brief Classes to manage home security sensors on Adafruit Funhouse
+ * @version 0.1
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "fh_homesec.h"
