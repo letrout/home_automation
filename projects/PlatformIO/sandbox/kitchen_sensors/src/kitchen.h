1350af40181e288ccdaf2ddd47715ad88080021b
Joel Luth <joelluth@gmail.com>
2023-12-23 09:40:07 -0600

    move door to D8 to make room for PIR and I2C

--- projects/PlatformIO/sandbox/kitchen_sensors/src/kitchen.h
+++ projects/PlatformIO/sandbox/kitchen_sensors/src/kitchen.h
@@ -1,9 +1,9 @@
 #ifndef SENSOR_DEVICE_H
 #define SENSOR_DEVICE_H 1
-#define LIBRARY 1
+#define KITCHEN 1
 
 
-const int door_pin = D1;
+const int door_pin = D8;
 const char* location = "Owens";
 const char* room = "kitchen";
 const char* room_loc = "deck";
